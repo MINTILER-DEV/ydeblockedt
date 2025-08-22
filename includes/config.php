addb3116aefbcc4dc2431e011b6d4dd93060eeb5
Antonio <64821480+MINTILER-DEV@users.noreply.github.com>
2025-08-21 22:42:18 -0500

    changed link

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,7 @@ define('NONE', 'AIzaSyD4KHNewNVn6bDGIHMX3TC2PlXwlbocW2M');
 define('API_KEY', 'AIzaSyBdCHy_9607c7yMxFOoKZ5LB7rHVsEaT8s');
 define('NONE2', 'AIzaSyCNmPWJLukDyNYxvjjlt4s70_h7Kp8PJho');
 
-define('YT_DLP_API', 'https://6eb59fbbbad6.ngrok-free.app');
+define('YT_DLP_API', 'https://9277a8bf2579.ngrok-free.app');
 
 define('CACHE_DIR', __DIR__ . '/../cache');
 define('CACHE_EXPIRE', 7200); // 2 hours in seconds
