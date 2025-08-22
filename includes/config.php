531fb15c556ca1b94598d77b512afd27b820b18c
Antonio <64821480+MINTILER-DEV@users.noreply.github.com>
2025-08-21 22:27:56 -0500

    updated api

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,7 @@ define('NONE', 'AIzaSyD4KHNewNVn6bDGIHMX3TC2PlXwlbocW2M');
 define('API_KEY', 'AIzaSyBdCHy_9607c7yMxFOoKZ5LB7rHVsEaT8s');
 define('NONE2', 'AIzaSyCNmPWJLukDyNYxvjjlt4s70_h7Kp8PJho');
 
-define('YT_DLP_API', 'https://2c21-2605-a601-ae33-8100-b4bd-ad7e-f0bc-2524.ngrok-free.app');
+define('YT_DLP_API', 'https://j2tux1z2yms8.share.zrok.io');
 
 define('CACHE_DIR', __DIR__ . '/../cache');
 define('CACHE_EXPIRE', 7200); // 2 hours in seconds
@@ -16,5 +16,5 @@ define('SERVER_DOWN', false);
 
 // Site Configuration
 define('SITE_NAME', 'YDeblockedT');
-define('SITE_DESCRIPTION', 'A modern YouTube experience with neon styling');
+define('SITE_DESCRIPTION', 'A modern YouTube experience styling');
 ?>
