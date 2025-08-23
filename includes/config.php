2e25d47a761c8c82be3ac3c7ddc621a04c1d6824
Antonio <64821480+MINTILER-DEV@users.noreply.github.com>
2025-08-23 09:50:12 -0500

    updated api again

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,7 @@ define('NONE', 'AIzaSyD4KHNewNVn6bDGIHMX3TC2PlXwlbocW2M');
 define('API_KEY', 'AIzaSyBdCHy_9607c7yMxFOoKZ5LB7rHVsEaT8s');
 define('NONE2', 'AIzaSyCNmPWJLukDyNYxvjjlt4s70_h7Kp8PJho');
 
-define('YT_DLP_API', 'https://d2bf4ec3dab3.ngrok-free.app');
+define('YT_DLP_API', 'https://70411a92c5d7.ngrok-free.app');
 
 define('CACHE_DIR', __DIR__ . '/../cache');
 define('CACHE_EXPIRE', 7200); // 2 hours in seconds
